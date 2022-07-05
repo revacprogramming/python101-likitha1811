be4860e9e2c271f63a5bbb1b4afe1a3298d23871
Likitha <97089754+likitha1811@users.noreply.github.com>
2022-07-05 00:51:31 +0000

    o

--- ActivitySet#1/problem#15.py
+++ ActivitySet#1/problem#15.py
@@ -1,2 +1,9 @@
 # Object Oriented Programming
 # https://www.py4e.com/lessons/Objects
+stuff = list()
+stuff.append('python')
+stuff.append('chuck')
+stuff.sort()
+print (stuff[0])
+print (stuff._getitem_(0))
+print (list._getitem_(stuff,0))
